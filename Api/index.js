ef16d185513f5bb8363918f4e422f0acfa0d6c13
francisamidu <francisamidu124@gmail.com>
2021-09-24 12:25:46 -0700

    added api key

--- Api/index.js
+++ Api/index.js
@@ -1,6 +1,7 @@
 class Api {
   static async getImages(term) {
     try {
+      const API_URL = `https://pixabay.com/api/?key=${process.env.PIXABAY_API_KEY}&q=${term}&image_type=photo`
       const request = await fetch(term)
       const response = await request.json()
       return response
