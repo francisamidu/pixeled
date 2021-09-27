2517737496fac5d1ae89d272363ca9fc9b8efd47
francisamidu <francisamidu124@gmail.com>
2021-09-27 13:31:18 -0700

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -3,8 +3,8 @@
 </template>
 
 <script>
-export default {
   import { mapActions } from 'vuex'
+export default {
   head() {
     return {
       title: 'Pixeled - An image gallery built on Nuxtjs',
