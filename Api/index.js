1f3701b2890eec37bb3b57ae89def535f0d4d3b6
francisamidu <francisamidu124@gmail.com>
2021-09-28 11:46:10 -0700

    Update index.js

--- Api/index.js
+++ Api/index.js
@@ -4,7 +4,7 @@ class Api {
   static async getImages(term) {
     try {
       const API_URL = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=${term}&image_type=photo`
-      const request = await fetch(term)
+      const request = await fetch(API_URL)
       const response = await request.json()
       return response
     } catch (error) {
